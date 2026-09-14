58edf08fb6f9187397fb4ec3c82c3167f3a31491
Udo <ues@udo-esser.de>
2026-09-14 23:50:34 +0200

    keyboard fixes return correct ASCII; defined chars for LCD; test loop kernel for keyboard echo to LCD

--- rom/os/src/lcd.s
+++ rom/os/src/lcd.s
@@ -18,6 +18,25 @@
 .export LCD_print_str
 .export LCD_print_char
 
+.macro DEC_CURSOR_PTR
+    .local dcp_col_zero
+    .local dcp_exit
+    lda ZP_LCD_COL
+    beq dcp_col_zero
+    dec
+    sta ZP_LCD_COL
+    bra dcp_exit
+dcp_col_zero:
+    ldx ZP_LCD_ROW
+    beq dcp_exit
+    dex
+    stx ZP_LCD_ROW
+    lda #LCDCOLS
+    dec
+    sta ZP_LCD_COL
+dcp_exit:
+.endmacro
+
 .segment "CODE"
 
 ;================================================================================
@@ -47,20 +66,9 @@ LCD_init:
     lda #%00000001 ; Clear screen
     jsr lcd_instruction
     
-    lda #$00
-    sta ZP_LCD_BUF_IDX
-    sta ZP_LCD_COL
-    sta ZP_LCD_ROW
-    jsr lcd_setcursor
-    
-    ; init lcd buffer
-    ldx #0
-    lda #$20  ; blank/space char
-@init_buf:
-    sta LCD_BUFFER, x
-    inx
-    cpx #LCDMAXCOL
-    bne @init_buf
+    jsr lcd_init_custom_chars   ; initialize custom characters in CGRAM
+
+    jsr LCD_clear
 
     ply
     plx
@@ -128,6 +136,37 @@ lcd_setup:
     rts
 
 ;================================================================================
+;   lcd_init_custom_char - Eigenes Zeichen (z.B. Backslash) ins CGRAM schreiben
+;   ————————————————————————————————————
+;   Parameters:      none
+;   Returned Values: none
+;   Destroys:        none
+;   ————————————————————————————————————
+;================================================================================
+lcd_init_custom_chars:
+    pha
+
+    ; 1. Ins CGRAM ab Adresse $40 wechseln
+    lda #$48           
+    jsr lcd_instruction     
+
+    ; 2. Schleife über alle 56 Bytes (7 Zeichen * 8 Bytes)
+    ldx #0                    ; X-Register als Index auf 0 setzen
+@loop:
+    lda custom_char_data, x   ; Byte aus der Tabelle laden
+    jsr lcd_writedata         ; An das LCD senden
+    inx                       ; Nächstes Byte
+    cpx #56                   ; Haben wir alle 32 Bytes gesendet?
+    bne @loop                 ; Wenn nein, Schleife wiederholen
+
+    ; 3. Zurück in den normalen Textmodus (DDRAM) schalten
+    lda #$80            
+    jsr lcd_instruction
+
+    pla
+    rts
+
+;================================================================================
 ;   lcd_instruction - send command to LCD
 ;   ————————————————————————————————————
 ;   Parameters:      .A command
@@ -136,8 +175,10 @@ lcd_setup:
 ;   ————————————————————————————————————
 ;================================================================================
 lcd_instruction:
-    jsr lcd_wait
     pha
+
+    jsr lcd_wait
+
     lsr
     lsr
     lsr
@@ -155,6 +196,40 @@ lcd_instruction:
     eor #E         ; Clear E bit
     sta LCD_PORT
     rts
+
+;================================================================================
+;   lcd_writedata - send data to LCD
+;   ————————————————————————————————————
+;   Parameters:      .A data
+;   Returned Values: none
+;   Destroys:        .A
+;   ————————————————————————————————————
+;================================================================================
+lcd_writedata:
+    pha
+
+    jsr lcd_wait
+    
+    lsr
+    lsr
+    lsr
+    lsr             ; Send high 4 bits
+    ora #RS         ; Set RS
+    sta LCD_PORT
+    ora #E          ; Set E bit to send instruction
+    sta LCD_PORT
+    eor #E          ; Clear E bit
+    sta LCD_PORT
+    pla
+    and #%00001111  ; Send low 4 bits
+    ora #RS         ; Set RS
+    sta LCD_PORT
+    ora #E          ; Set E bit to send instruction
+    sta LCD_PORT
+    eor #E          ; Clear E bit
+    sta LCD_PORT
+    rts
+
 ;================================================================================
 ;   lcd_wait - wait till LCD is not busy
 ;   ————————————————————————————————————
@@ -191,36 +266,6 @@ lcd_wait:
     rts
 
 ;================================================================================
-;   lcd_writedata - send data to LCD
-;   ————————————————————————————————————
-;   Parameters:      .A data
-;   Returned Values: none
-;   Destroys:        .A
-;   ————————————————————————————————————
-;================================================================================
-lcd_writedata:
-    pha
-    lsr
-    lsr
-    lsr
-    lsr             ; Send high 4 bits
-    ora #RS         ; Set RS
-    sta LCD_PORT
-    ora #E          ; Set E bit to send instruction
-    sta LCD_PORT
-    eor #E          ; Clear E bit
-    sta LCD_PORT
-    pla
-    and #%00001111  ; Send low 4 bits
-    ora #RS         ; Set RS
-    sta LCD_PORT
-    ora #E          ; Set E bit to send instruction
-    sta LCD_PORT
-    eor #E          ; Clear E bit
-    sta LCD_PORT
-    rts
-
-;================================================================================
 ;   LCD_clear - clears the LCD
 ;   ————————————————————————————————————
 ;   Parameters:      none
@@ -230,7 +275,19 @@ lcd_writedata:
 ;================================================================================
 LCD_clear:
     pha
+
+    ; init lcd buffer
+    ldx #0
+    lda #$20  ; blank/space char
+@init_buf:
+    sta LCD_BUFFER, x
+    inx
+    cpx #LCDMAXCOL
+    bne @init_buf
+
+    ; initbuffer index, set cursor to 0,0 and clear screen
     lda #$00
+    sta ZP_LCD_BUF_IDX
     sta ZP_LCD_COL
     sta ZP_LCD_ROW
     lda #%00000001 ; Clear screen
@@ -275,23 +332,14 @@ LCD_backspace:
     pha
     phx
     
-    lda ZP_LCD_COL
-    beq @colzero
-    dec
-    sta ZP_LCD_COL
-    bra @backspaceexit
-@colzero:
-    ldx ZP_LCD_ROW
-    beq @backspaceexit
-    dex
-    stx ZP_LCD_ROW
-    lda #LCDCOLS
-    dec
-    sta ZP_LCD_COL
-@backspaceexit:
+    DEC_CURSOR_PTR
+    jsr lcd_setcursor
+    lda #$20            ; blank
+    jsr lcd_writedata
+    jsr lcd_setcursor
+
     plx
     pla
-    jsr lcd_setcursor
     rts
 
 ;================================================================================
@@ -394,12 +442,11 @@ LCD_print_char:                 ; normaler Aufruf um ein Zeichen auf LCD zu schr
     plx
     pla
 
-lcd_print_char_from_write_buf:  ; Aufruf aus lcd_write_buf raus
+lcd_print_char_from_scroll:  ; Aufruf aus lcd_scroll raus
     pha
     phx
     
     jsr lcd_setcursor
-    jsr lcd_wait
     jsr lcd_writedata
     
     ; move cursor to next cell
@@ -485,7 +532,7 @@ lcd_scroll:
     ldx #LCD_FIRST_LINE
 @print_chars:
     lda LCD_BUFFER, x
-    jsr lcd_print_char_from_write_buf
+    jsr lcd_print_char_from_scroll
     inx
     cpx #LCDMAXSCROLL
     bne @print_chars
@@ -512,3 +559,82 @@ lcdbufrowstart:
     .byte 20
     .byte 40
     .byte 60
+
+custom_char_data:
+    ; Platz $01: Backslash (\)
+    .byte $10, $10, $08, $04, $02, $01, $01, $00
+    ; Platz $02: Großes Ä
+    .byte $0A, $00, $0E, $11, $1F, $11, $11, $00
+    ; Platz $03: Großes Ö
+    .byte $0A, $00, $0E, $11, $11, $11, $0E, $00
+    ; Platz $04: Großes Ü
+    .byte $0A, $00, $11, $11, $11, $11, $0E, $00
+    ; Platz $05: Euro sign €
+    .byte $07, $08, $1E, $08, $1E, $08, $07, $00
+    ; Platz $06: Euro sign €
+    .byte $06, $08, $04, $0A, $04, $02, $0C, $00
+    ; Platz $07: ´
+    .byte $01, $02, $04, $00, $00, $00, $00, $00
+
+; Reihe 0:  %00010000  (Hex: $10)   # . . .
+; Reihe 1:  %00010000  (Hex: $10)   # . . .
+; Reihe 2:  %00001000  (Hex: $08)   . # . .
+; Reihe 3:  %00000100  (Hex: $04)   . . # .
+; Reihe 4:  %00000010  (Hex: $02)   . . . #
+; Reihe 5:  %00000001  (Hex: $01)   . . . . #
+; Reihe 6:  %00000001  (Hex: $01)   . . . . #
+; Reihe 7:  %00000000  (Hex: $00)   (Cursor-Linie, bleibt meist frei)
+
+; Reihe 0:  %00001010  (Hex: $0A)   . # . # .  (Die Punkte)
+; Reihe 1:  %00000000  (Hex: $00)   . . . . .
+; Reihe 2:  %00001110  (Hex: $0E)   . # # # .
+; Reihe 3:  %00010001  (Hex: $11)   # . . . #
+; Reihe 4:  %00011111  (Hex: $1F)   # # # # #
+; Reihe 5:  %00010001  (Hex: $11)   # . . . #
+; Reihe 6:  %00010001  (Hex: $11)   # . . . #
+; Reihe 7:  %00000000  (Hex: $00)   . . . . .
+
+; Reihe 0:  %00001010  (Hex: $0A)   . # . # .
+; Reihe 1:  %00000000  (Hex: $00)   . . . . .
+; Reihe 2:  %00001110  (Hex: $0E)   . # # # .
+; Reihe 3:  %00010001  (Hex: $11)   # . . . #
+; Reihe 4:  %00010001  (Hex: $11)   # . . . #
+; Reihe 5:  %00010001  (Hex: $11)   # . . . #
+; Reihe 6:  %00001110  (Hex: $0E)   . # # # .
+; Reihe 7:  %00000000  (Hex: $00)   . . . . .
+
+; Reihe 0:  %00001010  (Hex: $0A)   . # . # .
+; Reihe 1:  %00000000  (Hex: $00)   . . . . .
+; Reihe 2:  %00010001  (Hex: $11)   # . . . #
+; Reihe 3:  %00010001  (Hex: $11)   # . . . #
+; Reihe 4:  %00010001  (Hex: $11)   # . . . #
+; Reihe 5:  %00010001  (Hex: $11)   # . . . #
+; Reihe 6:  %00001110  (Hex: $0E)   . # # # .
+; Reihe 7:  %00000000  (Hex: $00)   . . . . .
+
+; Reihe 0:  %00001010  (Hex: $07)   . . # # #
+; Reihe 1:  %00000000  (Hex: $08)   . # . . .
+; Reihe 2:  %00010001  (Hex: $1E)   # # # # .
+; Reihe 3:  %00010001  (Hex: $08)   . # . . .
+; Reihe 4:  %00010001  (Hex: $1E)   # # # # .
+; Reihe 5:  %00010001  (Hex: $08)   . # . . .
+; Reihe 6:  %00001110  (Hex: $07)   . . # # #
+; Reihe 7:  %00000000  (Hex: $00)   . . . . .
+
+; Reihe 0:  %00001010  (Hex: $06)   . . # # .
+; Reihe 1:  %00000000  (Hex: $08)   . # . . .
+; Reihe 2:  %00010001  (Hex: $04)   . . # . .
+; Reihe 3:  %00010001  (Hex: $0A)   . # . # .
+; Reihe 4:  %00010001  (Hex: $04)   . . # . .
+; Reihe 5:  %00010001  (Hex: $02)   . . . # .
+; Reihe 6:  %00001110  (Hex: $0C)   . # # . .
+; Reihe 7:  %00000000  (Hex: $00)   . . . . .
+
+; Reihe 0:  %00001010  (Hex: $01)   . . . . #
+; Reihe 1:  %00000000  (Hex: $02)   . . . # .
+; Reihe 2:  %00010001  (Hex: $04)   . . # . .
+; Reihe 3:  %00010001  (Hex: $00)   . . . . .
+; Reihe 4:  %00010001  (Hex: $00)   . . . . .
+; Reihe 5:  %00010001  (Hex: $00)   . . . . .
+; Reihe 6:  %00001110  (Hex: $00)   . . . . .
+; Reihe 7:  %00000000  (Hex: $00)   . . . . .
